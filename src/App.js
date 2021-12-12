664b3b4bbe6b12216d2aa35b06f54db34af9252c
Alex <lin87542296@gmail.com>
2021-12-12 22:49:54 +0800

    套版練習 更改<App/>

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import Navigation from "./Practice/Navigation";
+import Header from "./Practice/Header";
+import Service from "./Practice/Services";
+import Portfolio from "./Practice/Protfolio";
+import About from "./Practice/About";
+import Team from "./Practice/Team";
+import PY5 from "./Practice/PY5";
+import Contact from "./Practice/Contact";
+import Footer from "./Practice/Footer";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigation />
+      <Header />
+      <Service />
+      <Portfolio />
+      <About />
+      <Team />
+      <PY5 />
+      <Contact />
+      <Footer />
     </div>
   );
 }
